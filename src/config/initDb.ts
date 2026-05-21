a0d65eb1fc43610d81ec8ad27396dd3d0990014b
robinahmed12 <shawonahmed0294@gmail.com>
2026-05-21 18:11:58 +0600

    fixed db url

--- src/config/initDb.ts
+++ src/config/initDb.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const initDb = async (): Promise<void> => {
+export const initDb = async (): Promise<void> => {
   const client = await pool.connect();
 
   try {
