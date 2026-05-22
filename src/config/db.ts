b5958c17c24c7f461cb50d99fdd672611ee6b3ec
robinahmed12 <shawonahmed0294@gmail.com>
2026-05-22 10:55:01 +0600

    refactor configuration

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,10 +5,6 @@ dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
 });
 
 pool.on('connect', () => {
